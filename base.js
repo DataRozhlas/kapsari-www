1254ddd1ab9168833782044f732ac2a9c69da59a
Jan Cibulka <honza@datastory.cz>
2015-12-09 16:23:30 +0100

    Graf dospodu, popisky sloupců.

--- base.js
+++ base.js
@@ -1,4 +1,4 @@
-var map;
+var dates, map;
 
 var breaks = [0.283044, 0.397056, 0.555200, 0.985058, 254.421067]
 
@@ -51,7 +51,7 @@ var style = function () {
         ["<", "rate", breaks[0]]
         ],
       "paint": {
-        "fill-color": "#2c7bb6",
+        "fill-color": "#fee5d9",
         "fill-outline-color": "#d9d9d9",
         "fill-opacity": 0.8
     }
@@ -68,7 +68,7 @@ var style = function () {
         ["<", "rate", breaks[1]]
         ],
       "paint": {
-        "fill-color": "#abd9e9",
+        "fill-color": "#fcae91",
         "fill-outline-color": "#d9d9d9",
         "fill-opacity": 0.8
     }
@@ -85,7 +85,7 @@ var style = function () {
         ["<", "rate", breaks[2]]
         ],
       "paint": {
-        "fill-color": "#ffffbf",
+        "fill-color": "#fb6a4a",
         "fill-outline-color": "#d9d9d9",
         "fill-opacity": 0.8
     }
@@ -102,7 +102,7 @@ var style = function () {
         ["<", "rate", breaks[3]]
         ],
       "paint": {
-        "fill-color": "#fdae61",
+        "fill-color": "#de2d26",
         "fill-outline-color": "#d9d9d9",
         "fill-opacity": 0.8
     }
@@ -114,7 +114,7 @@ var style = function () {
       "source-layer": "oop_kapsari",
       "filter": [">=", "rate", breaks[3]],
       "paint": {
-        "fill-color": "#d7191c",
+        "fill-color": "#a50f15",
         "fill-outline-color": "#d9d9d9",
         "fill-opacity": 0.8
     }},
@@ -131,7 +131,7 @@ var style = function () {
 
 var drawChart = function(months) {
 		$("#chart").empty()
-		var dates = []
+		dates = []
 		months = JSON.parse(months.replace(/'/g, '"'))
 
 		for(var year=2013; year <= 2015; year++) {
@@ -139,36 +139,36 @@ var drawChart = function(months) {
 				dates.push(month + '-' + year)
 			}
 		}
-		var data = dates.map(function(date ,i) {
+		var data = dates.slice(0, 33).map(function(date ,i) {
 			return {'x': i, 'y': (months[date] || 0), 'label': date}
 		});
 
 		//chart
-            var w = 440;
+            var w = 900;
             var h = 150;
 
             var yscale = d3.scale.linear()
                             .domain([0, 154])
-                            .range([12, h])
+                            .range([1, h])
 
             var svg = d3.select("#chart")
                 .append("svg")
                 .attr("width", w)
-                .attr("height", h);
+                .attr("height", h + 25);
             
             svg.selectAll("rect")
                .data(data)
                .enter()
                .append("rect")
                .attr("x", function(d) {
-                return d.x * 17;
+                	return d.x * 25;
                })
                .attr("y", function(d) {
-                return h - yscale(d.y)
+                	return h - yscale(d.y)
                })
-               .attr("width", 16)
+               .attr("width", 23)
                .attr("height", function(d) {
-                return yscale(Math.max(0, d.y));
+                	return yscale(Math.max(0, d.y));
                })
                .attr("fill", "#4292c6")
 
@@ -180,18 +180,33 @@ var drawChart = function(months) {
                     return Math.max(0, d.y);
                 })
                 .attr("x", function(d, i) {
-                    return d.x * 17;
+                    return d.x * 25;
                 })
                 .attr("y", function(d) {
                     if (d != 0) {
-                        return h - yscale(d.y) + 8;
+                        return h - yscale(d.y) - 3;
                     } else {
                         return h - yscale(d.y) + 2
                     }
                 })
                 .attr("font-family", "sans-serif")
-                .attr("font-size", "10px")
-                .attr("fill", "#f7fbff")
+                .attr("font-size", "12px")
+                .attr("fill", "black")
+
+            svg.selectAll("text.datum")
+                .data(data)
+                .enter()
+                .append("text")
+                .text(function(d) {
+                    return d.label.split('-')[0] + '. ' + d.label.substr(d.label.length - 2);
+                })
+                .attr("x", function(d, i) {
+                    return d.x * 25;
+                })
+                .attr("y", h + 15)
+                .attr("font-family", "sans-serif")
+                .attr("font-size", "8px")
+                .attr("fill", "black")
     };
 
 var drawMap = function() {
@@ -200,7 +215,7 @@ var drawMap = function() {
 	map = new mapboxgl.Map({
 	  container: 'map',
 	  center: [14.46562, 50.05981],
-	  zoom: 7,
+	  zoom: 6,
 	  style: style()
 	});
 
